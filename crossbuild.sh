c184834773dbd54f8e1ac90ca0ce5786088fc74c
Martin Dyring-Andersen <martin@dyring-andersen.dk>
2019-03-29 11:46:47 +0100

    Build develop with go mod

--- crossbuild.sh
+++ crossbuild.sh
@@ -6,14 +6,15 @@ cd cosmos-sdk
 # Append crossbuild-cli target to Makefile
 cat << "EOF" >> Makefile
 
-crossbuild-cli: vendor-deps
-	env GOOS=windows GOARCH=amd64 go build  $(BUILD_FLAGS) -o build/windows/amd64/gaiacli.exe ./cmd/gaia/cmd/gaiacli
-	env GOOS=linux GOARCH=amd64 go build  $(BUILD_FLAGS) -o build/linux/amd64/gaiacli ./cmd/gaia/cmd/gaiacli
-	env GOOS=darwin GOARCH=amd64 go build  $(BUILD_FLAGS) -o build/darwin/amd64/gaiacli ./cmd/gaia/cmd/gaiacli
+crossbuild-cli: go.sum
+	env GOOS=windows GOARCH=amd64 go build -mod=readonly $(BUILD_FLAGS) -o build/windows/amd64/gaiacli.exe ./cmd/gaia/cmd/gaiacli
+	env GOOS=linux GOARCH=amd64 go build -mod=readonly $(BUILD_FLAGS) -o build/linux/amd64/gaiacli ./cmd/gaia/cmd/gaiacli
+	env GOOS=darwin GOARCH=amd64 go build -mod=readonly $(BUILD_FLAGS) -o build/darwin/amd64/gaiacli ./cmd/gaia/cmd/gaiacli
 EOF
 
-echo *** Start of modified Makefile ***
+echo "*** Start of modified Makefile ***"
 cat Makefile
-echo *** End of modified Makefile ***
+echo "*** End of modified Makefile ***"
 
+which go
 make crossbuild-cli
